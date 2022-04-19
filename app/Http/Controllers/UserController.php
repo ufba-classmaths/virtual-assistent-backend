4994215b815e3c56c380954916cc80ea927c9497
EliabeSilvaUfba <silva.eliabe@ufba.br>
2022-04-19 11:09:34 -0300

    updating UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -32,7 +32,6 @@ class UserController extends Controller
      */
     public function store(StoreUserRequest $request)
     {
-
         $payload = $request->all();
         $payload["password"] = bcrypt($payload["password"]);
         $user = User::create($payload);
@@ -61,12 +60,15 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        if($user){
+        if ($user) {
             $user->name = $request->input('name');
             $user->email = $request->input('email');
             $user->password = $request->input('password');
             $user->update();
+            return $this->success('User updated', $user);
         }
+
+        return $this->error('User not found', 404);
     }
 
     /**
