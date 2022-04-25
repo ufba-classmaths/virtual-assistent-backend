6b9b24be15ded097cdf1252ee30febc75a78cae0
Beatriz Santana <beatrizsecomp@gmail.com>
2022-04-25 08:33:04 -0300

    Update NlpTest.php

--- tests/Feature/NlpTest.php
+++ tests/Feature/NlpTest.php
@@ -49,7 +49,7 @@ class NlpTest extends TestCase
             ->post('/api/v3/nlp', $data);
 
         $responseJson = $response->json();
-        print_r($responseJson);
+        #print_r($responseJson);
         //assert
         $response->assertStatus(200);
     }
