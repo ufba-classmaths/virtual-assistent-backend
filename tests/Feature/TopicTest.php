aa0bd096a942706cec8d3b7c087e20f146af861b
saburabrina <sabrinascosme@gmail.com>
2022-04-25 11:59:43 -0300

    fix typo

--- tests/Feature/TopicTest.php
+++ tests/Feature/TopicTest.php
@@ -29,7 +29,7 @@ class TopicTest extends TestCase
                     "id",
                     "topic_id",
                     "description",
-                    "answare"
+                    "answer"
                 ]
             ],
             "children" => [
